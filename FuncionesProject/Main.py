6fcc8504aba5d7cbada9d9cecec69da67dafdffa
Brayan Hormiga <Brayannumero15@gmail.com>
2024-10-03 18:52:11 -0500

    se crea el proyecto con el archivo Main

---
+++ FuncionesProject/Main.py
@@ -0,0 +1,15 @@
+#Ejemplo de una Funcion
+def my_function():
+    print('Hello from a function')
+print('Hoy es el quiz')
+#Invocar la funcion
+my_function()
+
+#Ejemplo funcion con argumentos
+#Definir una funcion
+def mostrarNombre(nombre):
+    print('Su nombre es: '+nombre)
+nom = input('Ingrese su nombre: ')
+#Invocer la funcion
+mostrarNombre(nom)
+mostrarNombre('Daniela')
